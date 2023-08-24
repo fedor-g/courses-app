b16e54bdcda5cb7247640cd3d72f23b7e4c64ce9
gluf <fedor_glushchenko@epam.com>
2023-08-24 18:25:49 +0300

    Covered all api in services

--- src/services.ts
+++ src/services.ts
@@ -2,6 +2,8 @@ import { AuthType } from './store/authors/types';
 import { CourseType } from './store/courses/types';
 import { UserType } from './store/user/types';
 
+// Auth
+
 export async function login(user) {
 	let response;
 
@@ -82,6 +84,8 @@ export async function retrieveCoursesAndAuthors() {
 	return { courses: recievedCourses, auths: recievedAuthors };
 }
 
+// Courses
+
 export async function getCourses(): Promise<CourseType[]> {
 	let response;
 
@@ -155,6 +159,135 @@ export async function getFilteredCourses(
 	}));
 }
 
+export async function createCourse(
+	inputTitle: string,
+	inputDescription: string,
+	inputDuration: number,
+	inputAuthors: Array<string>,
+	token: string
+) {
+	let response;
+
+	const input = {
+		title: inputTitle,
+		description: inputDescription,
+		duration: inputDuration,
+		authors: inputAuthors,
+	};
+
+	try {
+		response = await fetch('http://localhost:4000/courses/add', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: token,
+			},
+			body: JSON.stringify(input),
+		});
+	} catch (error) {
+		console.error(error);
+		return false;
+	}
+
+	const data = await response.json();
+
+	if (data.successful === false) {
+		return false;
+	}
+	return true;
+}
+
+export async function getCourseById(id: string): Promise<CourseType> {
+	let response;
+
+	try {
+		response = await fetch('http://localhost:4000/courses/' + id);
+	} catch (error) {
+		console.error(error);
+		return;
+	}
+
+	const data = await response.json();
+
+	if (data.successful === false) {
+		return;
+	}
+
+	return {
+		id: data.result.id,
+		title: data.result.title,
+		description: data.result.description,
+		creationDate: data.result.creationDate,
+		duration: data.result.duration,
+		authors: data.result.authors,
+	};
+}
+
+export async function updateCourse(
+	inputId: string,
+	inputTitle: string,
+	inputDescription: string,
+	inputDuration: number,
+	inputAuthors: Array<string>,
+	token: string
+) {
+	let response;
+
+	const input = {
+		title: inputTitle,
+		description: inputDescription,
+		duration: inputDuration,
+		authors: inputAuthors,
+	};
+
+	try {
+		response = await fetch('http://localhost:4000/courses/' + inputId, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: token,
+			},
+			body: JSON.stringify(input),
+		});
+	} catch (error) {
+		console.error(error);
+		return false;
+	}
+
+	const data = await response.json();
+
+	if (data.successful === false) {
+		return false;
+	}
+	return true;
+}
+
+export async function deleteCourse(id: string, token: string) {
+	let response;
+
+	try {
+		response = await fetch('http://localhost:4000/courses/' + id, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: token,
+			},
+		});
+	} catch (error) {
+		console.error(error);
+		return false;
+	}
+
+	const data = await response.json();
+
+	if (data.successful === false) {
+		return false;
+	}
+	return true;
+}
+
+// Authors
+
 export async function getAuthors(): Promise<AuthType[]> {
 	let response;
 
@@ -175,6 +308,121 @@ export async function getAuthors(): Promise<AuthType[]> {
 		name: auth.name,
 	}));
 }
+
+export async function createAuthor(inputName: string, token: string) {
+	let response;
+
+	const input = {
+		name: inputName,
+	};
+
+	try {
+		response = await fetch('http://localhost:4000/authors/add', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: token,
+			},
+			body: JSON.stringify(input),
+		});
+	} catch (error) {
+		console.error(error);
+		return [];
+	}
+
+	const data = await response.json();
+
+	if (data.successful === false) {
+		return [];
+	}
+	return data.result.map((auth: AuthType) => ({
+		id: auth.id,
+		name: auth.name,
+	}));
+}
+
+export async function getAuthorById(id: string): Promise<AuthType> {
+	let response;
+
+	try {
+		response = await fetch('http://localhost:4000/authors/' + id);
+	} catch (error) {
+		console.error(error);
+		return;
+	}
+
+	const data = await response.json();
+
+	if (data.successful === false) {
+		return;
+	}
+	return {
+		id: data.id,
+		name: data.name,
+	};
+}
+
+export async function updateAuthor(
+	inputId: string,
+	inputName: string,
+	token: string
+) {
+	let response;
+
+	const user = {
+		name: inputName,
+	};
+
+	try {
+		response = await fetch('http://localhost:4000/authors/' + inputId, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: token,
+			},
+			body: JSON.stringify(user),
+		});
+	} catch (error) {
+		console.error(error);
+		return [];
+	}
+
+	const data = await response.json();
+
+	if (data.successful === false) {
+		return [];
+	}
+	return {
+		id: data.reslut.id,
+		name: data.result.name,
+	};
+}
+
+export async function deleteAuthor(inputId: string, token: string) {
+	let response;
+
+	try {
+		response = await fetch('http://localhost:4000/authors/' + inputId, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: token,
+			},
+		});
+	} catch (error) {
+		console.error(error);
+		return false;
+	}
+
+	const data = await response.json();
+
+	if (data.successful === false) {
+		return false;
+	}
+	return true;
+}
+
+// Users
 
 export async function checkMe(token: string): Promise<UserType> {
 	let response;
